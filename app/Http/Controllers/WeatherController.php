3d7cf65f96ae8dce1a6fb0021818b58e0251b504
xCharlotte <charlottevoskuilen@hotmail.com>
2018-12-04 14:28:41 +0100

    Cleaned up my controller. Deleted old not working methods :cow:

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,40 +7,15 @@ use GuzzleHttp\Client;
 use Carbon\Carbon;
 
 class WeatherController extends Controller
-{
-  public function index() {
-    $client = new Client();
-    $response = $client->get('http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam');
-    // echo $res->getStatusCode(); // 200
-    // echo $res->getBody();
-    
-    $xml = simplexml_load_string($response->getBody());
-    $json = json_encode($xml);
-    $array = json_decode($json,TRUE);
-    
-    return view('index', compact('array'));
-  }
-  
-  public function test() {
-    $client = new Client();
-    
-    $response = $client->get('https://api.buienradar.nl/data/public/2.0/jsonfeed');
-    
-    $data = $response->getBody();
-    $data = json_decode($data);
-    
-    return view('weather', array('data' => $data));
-  }
-  
+{  
   public function getWeatherData() {
     $client = new Client();
-  
-    $response = $client->get('http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam');
+    $response = $client->get('http://weerlive.nl/api/json-data-10min.php?key=2a9e41a474&locatie=creil');
   
     $data = $response->getBody();
     $data = json_decode($data);
     
-    $date = Carbon::now()->formatLocalized('%A %e %B %Y %H:%M');
+    $date = Carbon::now()->formatLocalized('%A %e %B %Y');
   
     return view('index', array('data' => $data), compact('date'));
   }
