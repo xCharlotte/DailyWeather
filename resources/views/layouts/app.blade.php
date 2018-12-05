0c2d310717909a325582ec3579b1536da98a2c4f
xCharlotte <charlottevoskuilen@hotmail.com>
2018-12-05 13:26:33 +0100

    Deleted unused code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,23 +26,6 @@
     <a href="#" data-toggle="modal" data-target="#info"><i class="fas fa-info-circle"></i></a>
   </div>
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
-
         <main class="py-4">
             @yield('content')
         </main>
