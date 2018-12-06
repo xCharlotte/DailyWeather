0ea9780b343c0880aee660e14714258903e3d799
xCharlotte <charlottevoskuilen@hotmail.com>
2018-12-06 14:46:39 +0100

    Added another function to parse the api data in the mailer and made it more dry and added some comments

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -12,16 +12,8 @@ use Mail;
 class WeatherController extends Controller
 {  
   public function getWeatherData() {
-    $plaats = Input::get('plaats');
-    if (!isset($plaats)) {
-      $plaats = 'Amsterdam'; 
-    }
-    
-    $client = new Client();
-    $response = $client->get('http://weerlive.nl/api/json-data-10min.php?key=2a9e41a474&locatie='.$plaats);
-  
-    $data = $response->getBody();
-    $data = json_decode($data);
+    $plaats = Input::get('plaats'); // To get the param input from the url
+    $data = $this->getApiData($plaats); // Call private function for the api data
     
     $date = Carbon::now()->formatLocalized('%A %e %B %Y');
   
@@ -29,10 +21,25 @@ class WeatherController extends Controller
   }
   
   public function sendEmail(Request $request) {  
-    $email = $request->input('email');
-    $data = ['body' => 'This is a test!'];
+    $email = $request->input('email'); // Get the email input from the form in the Index view 
+    $plaats = $request->input('plaats'); // Get the plaats input from the Index view 
     
-    Mail::to($email)->send(new WeatherMail($data));
-    return redirect('/');
+    $data = $this->getApiData($plaats); 
+    
+    Mail::to($email)->send(new WeatherMail($data)); // Send email from the weatherMail Mailer
+    return redirect('/?plaats='.$plaats); // Returns back to the 
+  }
+  
+  private function getApiData($plaats) { // Made private function so it can't be used in other classes
+    if (!isset($plaats)) {
+      $plaats = 'Amsterdam'; // If plaats does not exist, choose Amsterdam
+    }
+    
+    // Make api call 
+    $client = new Client();
+    $response = $client->get('http://weerlive.nl/api/json-data-10min.php?key=2a9e41a474&locatie='.$plaats);
+  
+    $data = $response->getBody(); // Get the body from request
+    return $data = json_decode($data); // Show data in JSON
   }
 }
